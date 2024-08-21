5500d0910da1e6f203a65e60f7bb850eb6cec905
EJD222 <eljdiaz500@gmail.com>
2024-08-21 20:48:41 +0800

    Add Module01-Exercise01 Files

---
+++ Module01Exercise01/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Module01Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public string WelcomeMessage { get; set; }
+        public string EditYourProfile { get; set; }
+        public FontAttributes BoldText { get; set; }
+        public double Heading1 { get; set; }
+        public double Heading2 { get; set; }
+        public double Text1 { get; set; }
+
+        // Property to bind the switch state
+        private bool _isDarkMode;
+        public bool IsDarkMode
+        {
+            get => _isDarkMode;
+            set
+            {
+                _isDarkMode = value;
+                OnPropertyChanged(nameof(IsDarkMode));
+                UpdateTheme();
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            WelcomeMessage = "Welcome User!";
+            EditYourProfile = "Edit Your Profile";
+            BoldText = FontAttributes.Bold;
+            Heading1 = 40;
+            Heading2 = 25;
+            Text1 = 16;
+            IsDarkMode = false; // Default to light mode
+            this.BindingContext = this;
+        }
+
+        private void OnDarkModeToggled(object sender, ToggledEventArgs e)
+        {
+            IsDarkMode = e.Value;
+        }
+
+        private void UpdateTheme()
+        {
+            if (IsDarkMode)
+            {
+                this.Resources["BackgroundColor"] = Colors.Black;
+                this.Resources["TextColor"] = Colors.White;
+                this.Resources["EntryBorderColor"] = Color.FromArgb("#502bd4");
+            }
+            else
+            {
+                this.Resources["BackgroundColor"] = Colors.White;
+                this.Resources["TextColor"] = Colors.Black;
+                this.Resources["EntryBorderColor"] = Colors.Black;
+            }
+        }
+
+        private void OnClickSubmit(object sender, EventArgs e)
+        {
+            this.Resources["DynamicMessage"] = "Saved successfully!";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Module 01 - Exercise 01 (PDC50-LAB)
+
+    Name of Student: Elmalia Jane S. Diaz
+    Year and Section: BSIT4A
+    Activity Title: Module 01 - Exercise 01
+    Date Submitted: August 21, 2024
+
+## User Profile Management Form
+### Source Code
+- [MainPage.xaml](Module01Exercise01/MainPage.xaml)
+- [MainPage.xaml.cs](Module01Exercise01/MainPage.xaml.cs)
+- [CurrentDateExtension.cs](Module01Exercise01/CurrentDateExtension.cs)
+
+## Light Mode (Not Saved)
+### Output Screenshots
+<p align="center">
+    <img src="LightModeNotSaved-1/App-1.png" alt="LightModeNotSaved-1" width="270" height="585">
+    <img src="LightModeNotSaved-1/App-2.png" alt="LightModeNotSaved-2" width="270" height="585">
+</p>
+<p align="center">
+    <img src="DarkModeNotSaved-1/App-1.png" alt="DarkModeNotSaved-1" width="270" height="585">
+    <img src="DarkModeNotSaved-1/App-2.png" alt="DarkModeNotSaved-2" width="270" height="585">
+</p>
